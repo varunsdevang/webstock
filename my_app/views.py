17aa51e1b252e97f8077815c1fab21244715686b
lazygod108 <varundevang@gmail.com>
2019-11-17 13:21:25 +0530

    login page done

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse
 from .models import User, Portfolio, Stock, Bonds, MutualFunds, HedgeFunds, Cash
+from django.template import RequestContext
+from django.utils import timezone
 
 
 # Create your views here.
@@ -13,4 +15,15 @@ def home(request):
         return render(request, 'login.html')
 
 
-
+def login(request):
+    if request.method == 'POST':
+        # print(request.POST['username'], request.POST['password'])
+        for i in User.objects.all():
+            if request.POST['username'] == i.user_Name:
+                print(i.user_Name,i.user_Password)
+                if request.POST['password'] == i.user_Password:
+                    response = render(request, 'base.html')
+                    response.set_cookie('last_connection', timezone.now())
+                    response.set_cookie('username', i.user_Name)
+                    return response
+    return redirect('/')
