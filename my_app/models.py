176bbf94fcab4833d8fe957d3417e748970387fc
lazygod108 <varundevang@gmail.com>
2019-11-17 21:14:25 +0530

    inv added

--- my_app/models.py
+++ my_app/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
     user_DOB = models.IntegerField(validators=[MinValueValidator(18), MaxValueValidator(99)])
     user_Email = models.EmailField()
     user_Password = models.CharField(max_length=20)
+    users=models.Manager()
 
     def __str__(self):
         return self.user_Name
@@ -20,6 +21,7 @@ class User(models.Model):
 class Portfolio(models.Model):
     user_Portfolio = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     port_Name = models.CharField(max_length=50)
+    ports=models.Manager()
 
     def __str__(self):
         return self.port_Name
@@ -30,38 +32,11 @@ class Investment(models.Model):
     investment_amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(sys.maxsize)])
     investment_date = models.DateField()
     investment_name = models.CharField(max_length=30, primary_key=True)
+    return_rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(200)])
+    investments = models.Manager()
 
     def __str__(self):
         return self.investment_name
 
 
-class Stock(Investment):
-    investment_name = "Stock"
-    stock_CODE = models.CharField(max_length=5)
-    expected_Return_Rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(200)])
 
-
-class MutualFunds(Investment):
-    investment_name = "MutualFunds"
-    fund_Name = models.CharField(max_length=50)
-    fund_Provider_Bank = models.CharField(max_length=50)
-    return_Rate = models.IntegerField(validators=[MaxValueValidator(50), MinValueValidator(5)])
-
-
-class HedgeFunds(Investment):
-    investment_name = "HedgeFunds"
-    fund_Name = models.CharField(max_length=50)
-    fund_Provider = models.CharField(max_length=50)
-    return_Rate = models.IntegerField(validators=[MaxValueValidator(50), MinValueValidator(5)])
-
-
-class Cash(Investment):
-    investment_name = "Cash"
-    pass
-
-
-class Bonds(Investment):
-    investment_name = "Bonds"
-    bond_Issuer = models.CharField(max_length=50)
-    bond_Interest_Rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(20)])
-    bond_maturity_date = models.DateField()
--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponse
-from .models import User, Portfolio, Stock, Bonds, MutualFunds, HedgeFunds, Cash
+from .models import User, Portfolio, Investment
 from django.template import RequestContext
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -10,9 +11,24 @@ from django.utils import timezone
 def home(request):
     if 'username' in request.COOKIES:
         # print(request.COOKIES)
-
-
-        return render(request, 'loggedin.html', )
+        try:
+            user = User.users.get(user_Name=request.COOKIES['username'])
+            for port in Portfolio.ports.all():
+                if port.user_Portfolio == user:
+                    port_name = Portfolio.ports.get(user_Portfolio=user)
+                    port_id = port.port_Name
+            invs = []
+            total = 0
+            for inv in Investment.investments.all():
+                print(inv,inv.investment_parent)
+                if inv.investment_parent == port_name:
+                    invs.append(inv)
+                    total += inv.investment_amount
+            context1 = {'invs': invs, 'total': total, 'user': user, 'name': port_id}
+            print(context1)
+            return render(request, 'loggedin.html', context=context1)
+        except ObjectDoesNotExist:
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
@@ -20,11 +36,11 @@ def home(request):
 def login(request):
     if request.method == 'POST':
         # print(request.POST['username'], request.POST['password'])
-        for i in User.objects.all():
+        for i in User.users.all():
             if request.POST['username'] == i.user_Name:
                 print(i.user_Name, i.user_Password)
                 if request.POST['password'] == i.user_Password:
-                    response = render(request, 'base.html', context={'username': i.user_Name})
+                    response = render(request, 'loggedin.html', context={'username': i.user_Name})
                     response.set_cookie('last_connection', timezone.now())
                     response.set_cookie('username', i.user_Name)
                     return response
