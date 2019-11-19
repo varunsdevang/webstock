3f26485864a80c3b8d95db8b7b3b02e1a5ed0850
lazygod108 <varundevang@gmail.com>
2019-11-19 14:32:14 +0530

    registration done

--- my_app/views.py
+++ my_app/views.py
@@ -10,9 +10,11 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     if 'username' in request.COOKIES:
-        # print(request.COOKIES)
+        print(request.COOKIES)
         try:
             user = User.users.get(user_Name=request.COOKIES['username'])
+            if not user:
+                return render(request, 'login.html')
             for port in Portfolio.ports.all():
                 if port.user_Portfolio == user:
                     port_name = Portfolio.ports.get(user_Portfolio=user)
@@ -20,11 +22,12 @@ def home(request):
             invs = []
             total = 0
             for inv in Investment.investments.all():
-                print(inv,inv.investment_parent)
+                print(inv, inv.investment_parent)
                 if inv.investment_parent == port_name:
                     invs.append(inv)
                     total += inv.investment_amount
             context1 = {'invs': invs, 'total': total, 'user': user, 'name': port_id}
+            print("--------------------context---------------------------")
             print(context1)
             return render(request, 'loggedin.html', context=context1)
         except ObjectDoesNotExist:
@@ -44,4 +47,28 @@ def login(request):
                     response.set_cookie('last_connection', timezone.now())
                     response.set_cookie('username', i.user_Name)
                     return response
-    return redirect('/')
+    return home(request)
+
+
+def register(request):
+    if request.method=="GET":
+        return render(request,'register.html')
+    if request.method=='POST':
+        form=request.POST
+        print(form)
+
+        for i in User.users.all():
+            if form['username']==i.user_Name:
+                message="User already Exists"
+                return render(request,'register.html',context={'message':message})
+        if int(form['age'])<18 or int(form['age'])>99:
+            message="invalid Age"
+            return render(request,'register.html',context={'message':message})
+        print(form)
+        user=User.users.create(user_Name=form['username'],user_DOB=form['age'],user_Password=form['password'],user_Email=form['emailid'])
+        Portfolio.ports.create(user_Portfolio=user,port_Name=form['portfolioname'])
+        return redirect('/')
+
+
+
+
